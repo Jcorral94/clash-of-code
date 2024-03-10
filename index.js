f9f98ce5500d490559e27eba4bba3d011d8bdfc2
Jay <38401811+Jcorral94@users.noreply.github.com>
2024-03-10 16:36:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // length of senetence but of words with no numbers.
 const str = readline().replace(/[0-9]/g, "").trim().split(" ").filter(word => word != "");
 console.log(str.length);
+
+// Reverse: M = 100 -> "1h 40m"
+const M = parseInt(readline());
+const seconds = 60;
+const hourSuffix = "h";
+const minuteSuffix = "m";
+const hours = Math.floor(M/seconds)
+const minutes = M%seconds;
+
+print(hours+hourSuffix+" "+ minutes+minuteSuffix);
+// End Reverse: M = 100 -> "1h 40m"
