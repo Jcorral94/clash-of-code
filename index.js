42c09703c94386830a3a949383a85a7dda61547e
Jay <38401811+Jcorral94@users.noreply.github.com>
2024-03-11 17:31:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -39,3 +39,14 @@ for(let i = 0; i < T.length; i++){
 console.log(c);
 
 // End Reverse: T = 01010000 T2 = 00000000 Answer: 01010000
+
+
+// Shortest Mode: n = 100 how many numbers up to 100 have can add up to 10 ie 64 = [6,4] = 100
+n=+readline()
+d=0
+for(i=0;i<n;i++){[...nums]=(""+i).split``
+c=nums.reduce((f,v)=>f+=Number(v),0)
+if(c==10)d+=1
+}
+print(d)
+// End Shortest Mode: n = 100 how many numbers up to 100 have can add up to 10 ie 64 = [6,4] = 100
